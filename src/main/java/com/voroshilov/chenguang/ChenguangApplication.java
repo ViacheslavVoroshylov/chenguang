675e9d26fe2aefded22fbe6754620701ee4f4570
ViacheslavVoroshylov <v.v.voroshylov@gmail.com>
2025-05-26 18:02:51 +0300

    Initial commit

---
+++ src/main/java/com/voroshilov/chenguang/ChenguangApplication.java
@@ -0,0 +1,13 @@
+package com.voroshilov.chenguang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenguangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChenguangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/voroshilov/chenguang/ChenguangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voroshilov.chenguang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChenguangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
