24979d625ee7728e5f59d97c81898e9a6c797111
ViacheslavVoroshylov <v.v.voroshylov@gmail.com>
2025-06-28 15:38:42 +0300

    Experimental UI realization

--- src/main/java/com/voroshilov/chenguang/ChenguangApplication.java
+++ src/main/java/com/voroshilov/chenguang/ChenguangApplication.java
@@ -1,13 +1,39 @@
 package com.voroshilov.chenguang;
 
-import org.springframework.boot.SpringApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class ChenguangApplication {
+public class ChenguangApplication extends Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ChenguangApplication.class, args);
+    private ConfigurableApplicationContext springContext;
+
+    @Override
+    public void init() {
+        springContext = new SpringApplicationBuilder(ChenguangApplication.class).run();
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/templates/main.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+
+        primaryStage.setTitle("Task Manager");
+        primaryStage.setScene(new Scene(fxmlLoader.load()));
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        springContext.close();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
